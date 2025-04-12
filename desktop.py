cb883b42e017e4edd788f98c238dbe5ed2529516
to-aoki <to-aoki@users.noreply.github.com>
2025-04-13 01:27:52 +0900

    mcp

--- desktop.py
+++ desktop.py
@@ -3,7 +3,6 @@ from streamlit_desktop_app import start_desktop_app
 # on ubuntu 24
 #  sudo apt-get install build-essential libgl1-mesa-dev
 #  pip install pyqt5 pyqtwebengine
-#  pip install pywebview
 #  pip install pywebview[qt]
 #  sudo apt install libcairo2-dev libxt-dev libgirepository-2.0-dev
 #  pip install pycairo PyGObject
