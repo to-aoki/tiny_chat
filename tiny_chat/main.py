d0143fb4df5d74dce7c969ea3161e2c6e0dc1150
to-aoki <to-aoki@users.noreply.github.com>
2025-05-15 20:48:29 +0900

    hide deploy

--- tiny_chat/main.py
+++ tiny_chat/main.py
@@ -16,17 +16,30 @@ def main():
     parser.add_argument("--server_mode", "-s", action="store_true", help="server mode (don't save settings to file)")
     args = parser.parse_args()
 
+    # Deployの非表示
+    hide_elements_style = """
+        <style>
+        .stAppDeployButton {
+                visibility: hidden;
+            }
+        </style>
+    """
+
     # データベースモードかチャットモードかを判定
     if args.database:
-        # データベースモード
         import streamlit as st
+        # データベースモード
         st.set_page_config(page_title="データベース", layout="wide")  # stの他コンポーネントの利用都合が不透明なので真っ先に呼ぶ
+        # CSSを適用
+        st.markdown(hide_elements_style, unsafe_allow_html=True)
         from tiny_chat.database.database import run_database_app
         run_database_app()
     else:
-        # チャットモード（デフォルト）
         import streamlit as st
+        # チャットモード（デフォルト）
         st.set_page_config(page_title="チャット", layout="wide")
+        # CSSを適用
+        st.markdown(hide_elements_style, unsafe_allow_html=True)
         from tiny_chat.chat.app import run_chat_app
         run_chat_app(server_mode=args.server_mode)
 
